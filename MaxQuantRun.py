82338281263ba73fbb8f0aee2097a76634d0770f
sharon klig <klshar89@gmail.com>
2019-01-17 16:22:29 +0200

    method was changed

--- MaxQuantRun.py
+++ MaxQuantRun.py
@@ -5,6 +5,10 @@ import regex as re
 import os.path
 from os.path import isfile, join
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+import Files_and_Constants as fc
+#import Files_test as fc
+
+
 
 
 
@@ -18,7 +22,6 @@ def create_xml_config_file(config_folder, params, numThreads, config_sample_fold
     # root = tree.getroot()
     root = tree.getroot()
 
-
     # raw files
     rawFilesobj = root.find('filePaths')
     rawFilesobj2 = rawFilesobj.findall('string')
@@ -26,7 +29,6 @@ def create_xml_config_file(config_folder, params, numThreads, config_sample_fold
     experiments2 = experiments.findall('string')
     for file_name, exp_name, i in zip([params[0], params[1], params[2]], ['a', 'b', 'c'], [0, 1, 2]):
          create_raw_file(file_name, rawFilesobj2, exp_name, experiments, i)
-
 
     #db files
     fasta_db = root.find('fastaFiles')
@@ -50,7 +52,6 @@ def create_raw_file(file_name, rawFilesobj, exp_name, experiments, i):
         experiments[i].text = exp_name
 
 
-
 def create_fasta_db_file(fasta_file, fasta_db):
     fastaFileInfo = ET.SubElement(fasta_db, "FastaFileInfo")
     d1 = ET.SubElement(fastaFileInfo, "fastaFilePath")
@@ -70,8 +71,6 @@ def create_fasta_db_file(fasta_file, fasta_db):
     ET.dump(fasta_db)
 
 
-
-
 def create_directories(directory, root):
     root.find('pluginFolder').text = directory
     root.find('tempFolder').text = directory
@@ -79,17 +78,8 @@ def create_directories(directory, root):
     root.find('fixedCombinedFolder').text = directory
 
 
-
-
-
-
 def running_maxquant_through_cmd (config_path):
     config_file = config_path + 'mqpar.xml'
-    maxquant_exe_path = '/share/apps/maxquant/maxquant-1.6.3.4/bin/MaxQuantCmd.exe'
-    #subprocess.call(['module load maxquant/maxquant-1.6.3.4'])      #remove when using cgi
-    subprocess.run(['mono', maxquant_exe_path, config_file])
-    #args = maxquant_exe_path + config_path
-    #subprocess.run(['maxquant', config_path])
-    #subprocess.call(args, stdout=NULL, stderr=NULL, shell=False)
-    #os.system(maxquant_exe_path + config_path)
+    subprocess.run(['mono', fc.maxquant_exe_path, config_file])
+    #subprocess.run(['C:/Users/Sharon/Desktop/MaxQuant_1.6.4.0_ybLYtwW/MaxQuant/MaxQuantCmd.exe', config_file])
 
